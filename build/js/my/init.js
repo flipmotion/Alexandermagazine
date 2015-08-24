86688d32e86e139b19a3b5a6325f4b3f82b61108
flipmotion <flipmotions@gmail.com>
2015-08-24 03:19:42 +0300

    not last UPD!!!65%
    not last UPD!!!65%

---
+++ build/js/my/init.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	// var owlMain = $('[data-item="corusel"]');
+	// owlMain.owlCarousel({
+	// 	loop:true,
+	// 	margin:0,
+	// 	nav:true,
+	// 	dots:false,
+	// 	items:1,
+	// 	autoplay:true,
+	// 	navText: [
+	// 	  "<i class='my-arrow-left'></i>",
+	// 	  "<i class='my-arrow-right'></i>"
+ //      ],
+	// 	dots: true
+	// });
+});
+
